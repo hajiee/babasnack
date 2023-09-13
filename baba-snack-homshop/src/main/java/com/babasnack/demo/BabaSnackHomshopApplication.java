ba74c735a30dce78941fc79d927543da88032838
hajiee <135810421+hajiee@users.noreply.github.com>
2023-09-13 14:50:04 +0900

    Add files via upload

---
+++ baba-snack-homshop/src/main/java/com/babasnack/demo/BabaSnackHomshopApplication.java
@@ -0,0 +1,13 @@
+package com.babasnack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabaSnackHomshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabaSnackHomshopApplication.class, args);
+	}
+
+}
---
+++ baba-snack-homshop/src/test/java/com/babasnack/demo/BabaSnackHomshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babasnack.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabaSnackHomshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
