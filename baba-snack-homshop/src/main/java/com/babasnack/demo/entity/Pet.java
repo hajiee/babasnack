3dcc58838d8eab71ad81f73b97799d3c6654bf00
user <user@DESKTOP-83GLAPA>
2023-09-14 15:46:05 +0900

    pet 날아간부분 보충

--- baba-snack-homshop/src/main/java/com/babasnack/demo/entity/Pet.java
+++ baba-snack-homshop/src/main/java/com/babasnack/demo/entity/Pet.java
@@ -1,5 +1,16 @@
 package com.babasnack.demo.entity;
 
-public class Pet {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pet {
+	private String petName;
+	private String username;
+	private String petType;
+	private Long petAge;
+	private Boolean petSex;
 }
