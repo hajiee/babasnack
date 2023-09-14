3d51a2dea7473086b37777b475471513308ba6ff
user <user@DESKTOP-83GLAPA>
2023-09-14 15:08:02 +0900

    펫 포토추가

---
+++ baba-snack-homshop/src/main/java/com/babasnack/demo/entity/PetPhoto.java
@@ -0,0 +1,16 @@
+package com.babasnack.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetPhoto {
+	private Long petProno;
+	private String petName;
+	private String username;
+	private String petImg;
+	private String petSaveImg;
+}
