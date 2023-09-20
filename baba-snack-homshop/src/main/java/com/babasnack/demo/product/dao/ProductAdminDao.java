50e80b5ed836fac40e112388fadd2809e92ab285
김혜지 <houiji76@naver.com>
2023-09-20 10:47:20 +0900

    product 수정중

--- baba-snack-homshop/src/main/java/com/babasnack/demo/product/dao/ProductAdminDao.java
+++ baba-snack-homshop/src/main/java/com/babasnack/demo/product/dao/ProductAdminDao.java
@@ -1,5 +1,31 @@
 package com.babasnack.demo.product.dao;
 
-public interface ProductAdminDao {
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Update;
 
+import com.babasnack.demo.product.dto.ProductDto;
+
+@Mapper
+public interface ProductAdminDao {
+	// 상품등록
+	@Insert("INSERT INTO product (productName, productNotice, productStock, productPrice, productSize) " +
+	        "VALUES (#{product.productName}, #{product.productNotice}, #{product.productStock}, " +
+	        "#{product.productPrice}, #{product.productSize})")
+	@Options(useGeneratedKeys = true, keyProperty = "product.pno")	//MyBatis에서 자동 생성된 키(auto-generated key) 값을 사용하기 위한 옵션
+	public Long addProduct(ProductDto.WriteP productDto);
+
+	// 상품수정
+	@Update("UPDATE product SET productName = #{product.productName}, " +
+	        "productNotice = #{product.productNotice}, " +
+	        "productStock = #{product.productStock}, " +
+	        "productPrice = #{product.productPrice}, " +
+	        "prodcutSize = #{prodcut.prodcutSize} WHERE pno = #{pno}")
+	public Long updateProduct(ProductDto.WriteP prodcutDto);
+	
+	// 상품삭제
+    @Delete("DELETE FROM product WHERE pno = #{pno}")
+    public Boolean deleteProduct(Long pno);
 }
--- baba-snack-homshop/src/main/java/com/babasnack/demo/product/dao/ProductDao.java
+++ baba-snack-homshop/src/main/java/com/babasnack/demo/product/dao/ProductDao.java
@@ -3,7 +3,6 @@ package com.babasnack.demo.product.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
@@ -15,13 +14,13 @@ import com.babasnack.demo.product.dto.ProductDto;
 public interface ProductDao {
 	// 상품목록
 	@Select("SELECT * FROM product")
-    List<Product> findAll();
+    public List<Product> findAll();
 
 	// 상품조회
     @Select("SELECT * FROM product WHERE productName = #{productName}")
-    Product findByProductName(@Param("productName") String productName);
+    public Product findByProductName(String productName);
 
     // 한 페이지당 상품 수
     @Select("SELECT * FROM (SELECT rownum as rnum, p.* FROM (SELECT /*+ index_desc(product product_pk_pno)*/ * FROM product) p WHERE rownum <= #{endRownum}) WHERE rnum >= #{startRownum}")
-    List<ProductDto.ListP> findPageOne(@Param("startRownum") Long startRownum, @Param("endRownum") Long endRownum);
+    public List<ProductDto.ListP> findPageOne(Long startRownum, Long endRownum);
 }
